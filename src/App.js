ec3bb121a4614a6ba16faa404159d77b380ccc11
Furkan <mfurkan.firuz@gmail.com>
2022-04-26 23:33:57 +0300

    cxvvv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import Header from "./Header";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const [calculate, setCalculate] = useState("");
+
+  const [result, setResult] = useState(0);
+
+  function calculateResult(symbol, userInput, currentResult) {
+    if (symbol === "toplama") {
+      let finalResult = parseInt(userInput) + parseInt(currentResult);
+      console.log("jfhahgf" + finalResult)
+      setResult(finalResult);
+    }
+    if (symbol == "çıkarma") {
+      let finalResult = parseInt(userInput) - parseInt(currentResult);
+
+      setResult(finalResult);
+    }
+
+    if (symbol == "çarpma") {
+      let finalResult = parseInt(userInput) * parseInt(currentResult);
+
+      setResult(finalResult);
+    }
+    if (symbol == "bölme") {
+      let finalResult = parseInt(userInput) / parseInt(currentResult);
+      setResult(finalResult);
+    }
+
+    console.log(symbol, userInput, currentResult)
+  }
+  console.log(result);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="main">
+        <div>
+          <h1>{result}</h1>
+          <input
+            className="input-2"
+            placeholder="sayı girin"
+            onChange={(e) => setNumber(e.target.value)}
+          />
+          <button
+            className="btn"
+            onClick={() => calculateResult(calculate, number, result)}
+          >
+            {" "}
+            Cal{" "}
+          </button>
+          <button className="btn" onClick={() => setCalculate("toplama")}>
+            {" "}
+            +{" "}
+          </button>
+          <button className="btn" onClick={() => setCalculate("çıkarma")}>
+            {" "}
+            -{" "}
+          </button>
+          <button className="btn" onClick={() => setCalculate("çarpma")}>
+            {" "}
+            x{" "}
+          </button>
+          <button className="btn" onClick={() => setCalculate("bölme")}>
+            {" "}
+            /{" "}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div style={{display:"flex", justifyContent:"center"}}>
+        <h1>My First Counter App</h1>
+    </div>
+  )
+}
+
+export default Header
